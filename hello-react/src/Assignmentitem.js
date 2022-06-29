e248763e06658b872befa7d3122f1b64b1c397b0
mari <uiop6452@gmail.com>
2022-06-29 22:04:21 +0900

    과제 추가

--- hello-react/src/Assignmentitem.js
+++ hello-react/src/Assignmentitem.js
@@ -2,12 +2,24 @@ import React from 'react'
 
 
 const Assignmentitem = ({data})=>{
+
+    const style = {
+        backgroundColor:'#999',
+        listStyle : 'none',
+        marginBottom: '5px',
+        padding: '10px',
+        fontWeight:500
+    }
     
     return (
         <>
-            <ul>
+            <ul style={{
+                backgroundColor : '#eee',
+                width:'300px',
+                padding: '10px'
+            }}>
                 {
-                    data.map(item => <li key={item.id}>{item.title}</li>)
+                    data.map(item => <li style={style} key={item.id}>{item.title}</li>)
                 }
             </ul>
         </>
