25b38f342ce53e33a96837ca018eaf7f1ce7eb41
mari <uiop6452@gmail.com>
2022-07-20 22:52:08 +0900

    수정

--- todo-app/src/modules/index.js
+++ todo-app/src/modules/index.js
@@ -1,8 +1,17 @@
 import { combineReducers } from "redux"; 
+import {configureStore} from "@reduxjs/toolkit"
 //combineReducers 리덕스에서 제공하는 함수 
 import counter from "./counter";
 import todos from "./todos";
 
+const store = configureStore({reducer : todos})
+
+store.dispatch(() => {
+  return {
+    type: 'todos/CHANGE_INPUT'
+  }
+})
+console.log(store.getState())
 const rootReducer = combineReducers({
     // counter,
     todos,
